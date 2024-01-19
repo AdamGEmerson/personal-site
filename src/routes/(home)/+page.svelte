e00f18e95113273dc95f63e4494ec9391c4df26f
Adam G. Emerson <adamgemerson@gmail.com>
2024-01-18 22:46:56 -0500

    adjustments to DOM content on render

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -152,19 +152,20 @@
 	<meta name="twitter:description" content="Software Engineer and Designer" />
 </svelte:head>
 
-{#if mounted}
-	<div class="w-full h-full flex items-center justify-around">
-		<div
-			in:slide={{ duration: 1000, easing: quintOut, axis: 'x' }}
-			class="grid grid-cols-6 grid-rows-6 md:grid-cols-16 md:grid-rows-4 gap-2 md:grid-flow-col grid-flow-row w-[90vw] h-[90vh] md:w-[80vw] md:h-[80vh]"
-		>
-			<!-- Heading -->
+<!-- Hidden For SEO / Accessibility w/ typing effect-->
+<h1 class="font-serif hidden">Adam G. Emerson</h1>
+<h2 class="hidden">Software Engineer and Designer</h2>
+
+<div class="w-full h-full flex items-center justify-around">
+	<div
+		class="grid grid-cols-6 grid-rows-6 md:grid-cols-16 md:grid-rows-4 gap-2 md:grid-flow-col grid-flow-row w-[90vw] h-[90vh] md:w-[80vw] md:h-[80vh]"
+	>
+		<!-- Heading -->
+		{#if mounted}
 			<div
+				in:slide={{ duration: 1000, easing: quintOut, axis: 'x' }}
 				class="col-span-full row-span-1 md:row-span-1 bg-stone-900 text-stone-300 p-8 order-first"
 			>
-				<!-- Hidden For SEO / Accessibility w/ typing effect-->
-				<h1 class="font-serif hidden">Adam G. Emerson</h1>
-
 				<h2
 					class="text-4xl sm:text-6xl lg:text-8xl transition-all font-serif flex items-end"
 					out:fly
@@ -185,103 +186,105 @@
 					</Marquee>
 				</div>
 			</div>
-			{#if innerWidth >= 768}
-				<LargeBento {featured} {mid} {preloadedImages} {timer} />
-			{:else}
-				<SmallBento {featured} {mid} {preloadedImages} {timer} />
-			{/if}
-		</div>
+		{/if}
+		{#if innerWidth >= 768}
+			<LargeBento {featured} {mid} {preloadedImages} {timer} />
+		{:else}
+			<SmallBento {featured} {mid} {preloadedImages} {timer} />
+		{/if}
 	</div>
-	<!--			&lt;!&ndash; Bento &ndash;&gt;-->
-	<!--			<div class="col-span-full row-span-2 order-3 md:col-span-7 md:row-span-3 md:order-none">-->
-	<!--				{#each featured as item}-->
-	<!--					<a-->
-	<!--						in:blur|global={{ easing: quintOut, duration: 1000 }}-->
-	<!--						data-flip-id={item._id}-->
-	<!--						href={`/${item.isResearch ? 'research' : 'projects'}/${item.slug}`}-->
-	<!--						class="bento-item w-full h-full p-4 bg-stone-900 text-stone-300 hover:bg-stone-900/95 hover:cursor-pointer flex flex-col gap-2 justify-start"-->
-	<!--					>-->
-	<!--						<div class={'flex'}>-->
-	<!--							<div class="flex-1">-->
-	<!--								<h2 class="font-serif text-xl">{item.title}</h2>-->
-	<!--								<p class="font-mono text-sm h-16">{item.oneLiner}</p>-->
-	<!--							</div>-->
-	<!--							{#if item.isResearch}-->
-	<!--								<IconMicroscope class="flex-0 h-8 w-8 text-stone-600 self-end" />-->
-	<!--							{:else}-->
-	<!--								<IconGeometry class="flex-0 h-8 w-8 text-stone-600 self-end" />-->
-	<!--							{/if}-->
-	<!--						</div>-->
-	<!--						<TimerBar {timer} />-->
-	<!--						{#key item}-->
-	<!--							<div class="h-full">-->
-	<!--								<div-->
-	<!--									in:slide={{ delay: 600, duration: 1000, easing: quintOut }}-->
-	<!--									class="w-full h-full bg-teal-900"-->
-	<!--								>-->
-	<!--									<img-->
-	<!--										src={preloadedImages[item._id].src}-->
-	<!--										alt={preloadedImages[item._id].alt}-->
-	<!--										class="h-full w-full border-2 border-stone-300 object-cover"-->
-	<!--									/>-->
-	<!--								</div>-->
-	<!--							</div>-->
-	<!--						{/key}-->
-	<!--					</a>-->
-	<!--				{/each}-->
-	<!--			</div>-->
+</div>
+<!--			&lt;!&ndash; Bento &ndash;&gt;-->
+<!--			<div class="col-span-full row-span-2 order-3 md:col-span-7 md:row-span-3 md:order-none">-->
+<!--				{#each featured as item}-->
+<!--					<a-->
+<!--						in:blur|global={{ easing: quintOut, duration: 1000 }}-->
+<!--						data-flip-id={item._id}-->
+<!--						href={`/${item.isResearch ? 'research' : 'projects'}/${item.slug}`}-->
+<!--						class="bento-item w-full h-full p-4 bg-stone-900 text-stone-300 hover:bg-stone-900/95 hover:cursor-pointer flex flex-col gap-2 justify-start"-->
+<!--					>-->
+<!--						<div class={'flex'}>-->
+<!--							<div class="flex-1">-->
+<!--								<h2 class="font-serif text-xl">{item.title}</h2>-->
+<!--								<p class="font-mono text-sm h-16">{item.oneLiner}</p>-->
+<!--							</div>-->
+<!--							{#if item.isResearch}-->
+<!--								<IconMicroscope class="flex-0 h-8 w-8 text-stone-600 self-end" />-->
+<!--							{:else}-->
+<!--								<IconGeometry class="flex-0 h-8 w-8 text-stone-600 self-end" />-->
+<!--							{/if}-->
+<!--						</div>-->
+<!--						<TimerBar {timer} />-->
+<!--						{#key item}-->
+<!--							<div class="h-full">-->
+<!--								<div-->
+<!--									in:slide={{ delay: 600, duration: 1000, easing: quintOut }}-->
+<!--									class="w-full h-full bg-teal-900"-->
+<!--								>-->
+<!--									<img-->
+<!--										src={preloadedImages[item._id].src}-->
+<!--										alt={preloadedImages[item._id].alt}-->
+<!--										class="h-full w-full border-2 border-stone-300 object-cover"-->
+<!--									/>-->
+<!--								</div>-->
+<!--							</div>-->
+<!--						{/key}-->
+<!--					</a>-->
+<!--				{/each}-->
+<!--			</div>-->
 
-	<!--			{#each mid as item, i}-->
-	<!--				{#if i !== mid.length - 1}-->
-	<!--					<div-->
-	<!--						class={`col-span-3 row-span-1 order-${4 + i} md:col-span-4 md:row-span-1 md:order-none`}-->
-	<!--					>-->
-	<!--						<BentoItem-->
-	<!--							name={item.title}-->
-	<!--							description={item.oneLiner}-->
-	<!--							bentoId={item._id}-->
-	<!--							isResearch={item.isResearch}-->
-	<!--							href={`/${item.isResearch ? 'research' : 'projects'}/${item.slug}`}-->
-	<!--						/>-->
-	<!--					</div>-->
-	<!--				{:else}-->
-	<!--					<div class="col-span-3 row-span-1 md:col-span-7 md:row-span-1 order-5 md:order-none">-->
-	<!--						<BentoItem-->
-	<!--							name={item.title}-->
-	<!--							description={item.oneLiner}-->
-	<!--							bentoId={item._id}-->
-	<!--							isResearch={item.isResearch}-->
-	<!--							href={`/${item.isResearch ? 'research' : 'projects'}/${item.slug}`}-->
-	<!--						/>-->
-	<!--					</div>-->
-	<!--				{/if}-->
-	<!--			{/each}-->
-	<!--			<div class="col-span-3 row-span-2 md:order-none order-4">-->
-	<!--				<a-->
-	<!--					in:blur|global={{ easing: quintOut, duration: 1000 }}-->
-	<!--					class="w-full h-full p-4 bg-stone-900 text-stone-300 hover:bg-stone-900/95 hover:scale-[101%] hover:cursor-pointer flex justify-around relative overflow-hidden group"-->
-	<!--					href={'/projects'}-->
-	<!--				>-->
-	<!--					<div class="flex-1 z-10">-->
-	<!--						<h2 class="font-serif text-xl z-10">More Projects</h2>-->
-	<!--						<p class="font-mono text-sm z-10">All the other stuff that doesn't fit on this page</p>-->
-	<!--					</div>-->
-	<!--					<IconGeometry-->
-	<!--						class="z-0 absolute block -bottom-10 left-50 h-96 w-96 text-stone-800 group-hover:text-stone-700"-->
-	<!--					/>-->
-	<!--				</a>-->
-	<!--			</div>-->
-	<!--			<div class="order-2 col-span-2 row-span-1 md:order-none md:col-span-2 md:row-span-1">-->
-	<!--				<BentoItem name="github" variant="icon" />-->
-	<!--			</div>-->
-	<!--			<div class="order-2 col-span-2 row-span-1 md:order-none md:col-span-2 md:row-span-1">-->
-	<!--				<BentoItem name="linkedin" variant="icon" />-->
-	<!--			</div>-->
-	<!--			<div class="order-2 col-span-2 row-span-1 md:order-none md:col-span-2 md:row-span-1">-->
-	<!--				<BentoItem name="dribbble" variant="icon" />-->
-	<!--			</div>-->
-	<!--		</div>-->
-	<!--	</div>-->
+<!--			{#each mid as item, i}-->
+<!--				{#if i !== mid.length - 1}-->
+<!--					<div-->
+<!--						class={`col-span-3 row-span-1 order-${4 + i} md:col-span-4 md:row-span-1 md:order-none`}-->
+<!--					>-->
+<!--						<BentoItem-->
+<!--							name={item.title}-->
+<!--							description={item.oneLiner}-->
+<!--							bentoId={item._id}-->
+<!--							isResearch={item.isResearch}-->
+<!--							href={`/${item.isResearch ? 'research' : 'projects'}/${item.slug}`}-->
+<!--						/>-->
+<!--					</div>-->
+<!--				{:else}-->
+<!--					<div class="col-span-3 row-span-1 md:col-span-7 md:row-span-1 order-5 md:order-none">-->
+<!--						<BentoItem-->
+<!--							name={item.title}-->
+<!--							description={item.oneLiner}-->
+<!--							bentoId={item._id}-->
+<!--							isResearch={item.isResearch}-->
+<!--							href={`/${item.isResearch ? 'research' : 'projects'}/${item.slug}`}-->
+<!--						/>-->
+<!--					</div>-->
+<!--				{/if}-->
+<!--			{/each}-->
+<!--			<div class="col-span-3 row-span-2 md:order-none order-4">-->
+<!--				<a-->
+<!--					in:blur|global={{ easing: quintOut, duration: 1000 }}-->
+<!--					class="w-full h-full p-4 bg-stone-900 text-stone-300 hover:bg-stone-900/95 hover:scale-[101%] hover:cursor-pointer flex justify-around relative overflow-hidden group"-->
+<!--					href={'/projects'}-->
+<!--				>-->
+<!--					<div class="flex-1 z-10">-->
+<!--						<h2 class="font-serif text-xl z-10">More Projects</h2>-->
+<!--						<p class="font-mono text-sm z-10">All the other stuff that doesn't fit on this page</p>-->
+<!--					</div>-->
+<!--					<IconGeometry-->
+<!--						class="z-0 absolute block -bottom-10 left-50 h-96 w-96 text-stone-800 group-hover:text-stone-700"-->
+<!--					/>-->
+<!--				</a>-->
+<!--			</div>-->
+<!--			<div class="order-2 col-span-2 row-span-1 md:order-none md:col-span-2 md:row-span-1">-->
+<!--				<BentoItem name="github" variant="icon" />-->
+<!--			</div>-->
+<!--			<div class="order-2 col-span-2 row-span-1 md:order-none md:col-span-2 md:row-span-1">-->
+<!--				<BentoItem name="linkedin" variant="icon" />-->
+<!--			</div>-->
+<!--			<div class="order-2 col-span-2 row-span-1 md:order-none md:col-span-2 md:row-span-1">-->
+<!--				<BentoItem name="dribbble" variant="icon" />-->
+<!--			</div>-->
+<!--		</div>-->
+<!--	</div>-->
+{#if mounted}
 	<div
 		class="text-stone-900 z-50 fixed -bottom-14 -left-10 sm:-bottom-20 sm:-left-20"
 		transition:fly={{ duration: 1000, delay: 3600, x: -100, y: 100 }}
